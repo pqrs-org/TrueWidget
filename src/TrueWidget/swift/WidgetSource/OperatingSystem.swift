fa5538bd5d711b4ecd211c515e9ef2916a17c209
Takayama Fumihiko <tekezo@pqrs.org>
2022-12-28 08:19:11 +0900

    Use gethostname instead of ProcessInfo.processInfo.hostName

--- src/TrueWidget/swift/WidgetSource/OperatingSystem.swift
+++ src/TrueWidget/swift/WidgetSource/OperatingSystem.swift
@@ -5,8 +5,10 @@ extension WidgetSource {
   public class OperatingSystem: ObservableObject {
     static let shared = OperatingSystem()
 
-    @Published public var version: String
-    @Published public var hostName: String
+    @Published public var version = ""
+    @Published public var hostName = ""
+
+    private var timer: Timer?
 
     private init() {
       let operatingSystemVersion = ProcessInfo.processInfo.operatingSystemVersion
@@ -17,11 +19,36 @@ extension WidgetSource {
         operatingSystemVersion.patchVersion
       )
 
-      let name = ProcessInfo.processInfo.hostName
-      if let index = name.firstIndex(of: ".") {
-        hostName = String(name[...index].dropLast())
-      } else {
-        hostName = name
+      timer = Timer.scheduledTimer(
+        withTimeInterval: 3.0,
+        repeats: true
+      ) { [weak self] (_: Timer) in
+        guard let self = self else { return }
+
+        self.update()
+      }
+
+      self.update()
+    }
+
+    private func update() {
+      if !UserSettings.shared.showHostName {
+        return
+      }
+
+      // `ProcessInfo.processInfo.hostName` is not reflected the host name changes after the application is launched.
+      // So, we have to use `gethostname`.`
+      let length = 128
+      var buffer = [CChar](repeating: 0, count: length)
+      let error = gethostname(&buffer, length)
+      if error == 0 {
+        if let name = String(utf8String: buffer) {
+          if let index = name.firstIndex(of: ".") {
+            hostName = String(name[...index].dropLast())
+          } else {
+            hostName = name
+          }
+        }
       }
     }
   }
