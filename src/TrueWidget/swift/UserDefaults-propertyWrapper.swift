cb0227bb7996da902827c4f387a1a65d5b4d458c
Takayama Fumihiko <tekezo@pqrs.org>
2023-01-29 13:10:05 +0900

    Add UserDefaultJson

--- src/TrueWidget/swift/UserDefaults-propertyWrapper.swift
+++ src/TrueWidget/swift/UserDefaults-propertyWrapper.swift
@@ -20,3 +20,38 @@ struct UserDefault<T> {
     }
   }
 }
+
+@propertyWrapper
+struct UserDefaultJson<T: Codable> {
+  let key: String
+  let defaultValue: T
+
+  init(_ key: String, defaultValue: T) {
+    self.key = key
+    self.defaultValue = defaultValue
+  }
+
+  var wrappedValue: T {
+    get {
+      if let data = UserDefaults.standard.data(forKey: key) {
+        do {
+          let decoder = JSONDecoder()
+          return try decoder.decode(T.self, from: data)
+        } catch {
+          print("Failed to decode: (\(error))")
+        }
+      }
+
+      return defaultValue
+    }
+    nonmutating set {
+      do {
+        let encoder = JSONEncoder()
+        let data = try encoder.encode(newValue)
+        UserDefaults.standard.set(data, forKey: key)
+      } catch {
+        print("Failed to encode: (\(error))")
+      }
+    }
+  }
+}
