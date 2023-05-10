2666f9ecf75580c300c9599c71bcf7864a776310
Takayama Fumihiko <tekezo@pqrs.org>
2023-05-10 23:23:17 +0900

    Add deprecated annotation

--- src/Helper/DeprecatedOpenAtLogin/objc/DeprecatedOpenAtLoginObjc.h
+++ src/Helper/DeprecatedOpenAtLogin/objc/DeprecatedOpenAtLoginObjc.h
@@ -4,9 +4,13 @@
 
 @interface DeprecatedOpenAtLoginObjc : NSObject
 
-+ (BOOL)registered:(NSURL *)appURL;
++ (BOOL)registered:(NSURL *)appURL
+    API_DEPRECATED("Use SMAppService", macos(10.5, 12.0));
 
-+ (void)register:(NSURL *)appURL;
-+ (void)unregister:(NSURL *)appURL;
++ (void)register:(NSURL *)appURL
+    API_DEPRECATED("Use SMAppService", macos(10.5, 12.0));
+
++ (void)unregister:(NSURL *)appURL
+    API_DEPRECATED("Use SMAppService", macos(10.5, 12.0));
 
 @end
