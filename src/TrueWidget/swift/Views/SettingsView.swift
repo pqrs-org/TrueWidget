5833de00b70e77bc1abb9d914e795c54239147de
Takayama Fumihiko <tekezo@pqrs.org>
2025-01-02 10:58:38 +0900

    Switch to using TabView

--- src/TrueWidget/swift/Views/SettingsView.swift
+++ src/TrueWidget/swift/Views/SettingsView.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-enum NavigationTag: String {
+enum TabTag: String {
   case main
   case operatingSystem
   case xcode
@@ -11,116 +11,53 @@ enum NavigationTag: String {
 }
 
 struct SettingsView: View {
-  @ObservedObject private var userSettings = UserSettings.shared
-  @State private var selection: NavigationTag = .main
+  @Binding var showMenuBarExtra: Bool
+
+  @State private var selection: TabTag = .main
 
   var body: some View {
-    HStack {
-      VStack(alignment: .leading, spacing: 0) {
-        Button(
-          action: {
-            selection = .main
-          },
-          label: {
-            SidebarLabelView(text: "Main", systemImage: "gear")
-          }
-        )
-        .sidebarButtonStyle(selected: selection == .main)
+    TabView(selection: $selection) {
+      SettingsMainView(showMenuBarExtra: $showMenuBarExtra)
+        .tabItem {
+          Label("Main", systemImage: "gear")
+        }
+        .tag(TabTag.main)
 
-        Button(
-          action: {
-            selection = .operatingSystem
-          },
-          label: {
-            SidebarLabelView(text: "Layout > Operation System", systemImage: "cube")
-          }
-        )
-        .sidebarButtonStyle(selected: selection == .operatingSystem)
+      SettingsCPUUsageView()
+        .tabItem {
+          Label("CPU", systemImage: "cube")
+        }
+        .tag(TabTag.cpuUsage)
 
-        Button(
-          action: {
-            selection = .xcode
-          },
-          label: {
-            SidebarLabelView(text: "Layout > Xcode", systemImage: "cube")
-          }
-        )
-        .sidebarButtonStyle(selected: selection == .xcode)
+      SettingsTimeView()
+        .tabItem {
+          Label("Time", systemImage: "cube")
+        }
+        .tag(TabTag.time)
 
-        Button(
-          action: {
-            selection = .cpuUsage
-          },
-          label: {
-            SidebarLabelView(text: "Layout > CPU Usage", systemImage: "cube")
-          }
-        )
-        .sidebarButtonStyle(selected: selection == .cpuUsage)
+      SettingsOperatingSystemView()
+        .tabItem {
+          Label("System", systemImage: "cube")
+        }
+        .tag(TabTag.operatingSystem)
 
-        Button(
-          action: {
-            selection = .time
-          },
-          label: {
-            SidebarLabelView(text: "Layout > Time", systemImage: "cube")
-          }
-        )
-        .sidebarButtonStyle(selected: selection == .time)
+      SettingsXcodeView()
+        .tabItem {
+          Label("Xcode", systemImage: "cube")
+        }
+        .tag(TabTag.xcode)
 
-        Button(
-          action: {
-            selection = .update
-          },
-          label: {
-            SidebarLabelView(text: "Update", systemImage: "network")
-          }
-        )
-        .sidebarButtonStyle(selected: selection == .update)
+      SettingsUpdateView()
+        .tabItem {
+          Label("Update", systemImage: "network")
+        }
+        .tag(TabTag.update)
 
-        Divider()
-          .padding(.vertical, 10.0)
-
-        Button(
-          action: {
-            selection = .action
-          },
-          label: {
-            SidebarLabelView(text: "Quit, Restart", systemImage: "bolt.circle")
-          }
-        )
-        .sidebarButtonStyle(selected: selection == .action)
-
-        Spacer()
-      }
-      .frame(width: 250)
-
-      Divider()
-
-      switch selection {
-      case .main:
-        SettingsMainView()
-      case .operatingSystem:
-        SettingsOperatingSystemView()
-      case .xcode:
-        SettingsXcodeView()
-      case .cpuUsage:
-        SettingsCPUUsageView()
-      case .time:
-        SettingsTimeView()
-      case .update:
-        SettingsUpdateView()
-      case .action:
-        SettingsActionView()
-      }
+      SettingsActionView()
+        .tabItem {
+          Label("Quit, Restart", systemImage: "bolt.circle")
+        }
+        .tag(TabTag.action)
     }
-    .padding()
-    .frame(width: 800)
-  }
-}
-
-struct SettingsView_Previews: PreviewProvider {
-  static var previews: some View {
-    SettingsView()
-      .previewLayout(.sizeThatFits)
   }
 }
