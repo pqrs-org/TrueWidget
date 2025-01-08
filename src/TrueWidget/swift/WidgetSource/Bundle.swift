797371aa2d824c159b4539f798df551b752d707d
Takayama Fumihiko <tekezo@pqrs.org>
2025-01-09 08:49:31 +0900

    Use URL.appending

--- src/TrueWidget/swift/WidgetSource/Bundle.swift
+++ src/TrueWidget/swift/WidgetSource/Bundle.swift
@@ -13,7 +13,12 @@ extension WidgetSource {
         // Once a Bundle instance is created and associated with a url (or more precisely, a path), that instance continues to be reused.
         // As a result, even if the version or other information is updated later, outdated information will still be returned.
         // Therefore, instead of using Bundle, retrieve the information directly from Info.plist.
-        let plistPath = "\(url.path)/Contents/Info.plist"
+        let plistPath =
+          url
+          .appending(component: "Contents", directoryHint: .isDirectory)
+          .appending(component: "Info.plist", directoryHint: .notDirectory)
+          .path
+
         guard let plistData = FileManager.default.contents(atPath: plistPath),
           let plistDict = try? PropertyListSerialization.propertyList(
             from: plistData, options: [], format: nil) as? [String: Any],
