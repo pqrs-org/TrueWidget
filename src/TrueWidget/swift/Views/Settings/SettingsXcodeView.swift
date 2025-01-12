44b3f9ffdd05a0178caaf7191e24dc9b01eac6d3
Takayama Fumihiko <tekezo@pqrs.org>
2025-01-12 21:50:16 +0900

    Adjust SettingsXcodeView layout

--- src/TrueWidget/swift/Views/Settings/SettingsXcodeView.swift
+++ src/TrueWidget/swift/Views/Settings/SettingsXcodeView.swift
@@ -7,14 +7,11 @@ struct SettingsXcodeView: View {
     VStack(alignment: .leading, spacing: 25.0) {
       GroupBox(label: Text("Xcode")) {
         VStack(alignment: .leading) {
-          HStack {
-            Toggle(isOn: $userSettings.showXcode) {
-              Text("Show Xcode bundle path")
-            }
-            .switchToggleStyle()
-
-            Spacer()
+          Toggle(isOn: $userSettings.showXcode) {
+            Text("Show Xcode bundle path")
           }
+          .switchToggleStyle()
+          .frame(maxWidth: .infinity, alignment: .leading)
 
           HStack {
             Text("Font size: ")
@@ -29,9 +26,8 @@ struct SettingsXcodeView: View {
             Text("pt")
 
             Text("(Default: 12 pt)")
-
-            Spacer()
           }
+          .frame(maxWidth: .infinity, alignment: .leading)
         }.padding()
       }
     }
