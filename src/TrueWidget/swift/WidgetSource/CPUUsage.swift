bcae74a64b388476c52a28e1f299f1e0fad8d09c
Takayama Fumihiko <tekezo@pqrs.org>
2025-03-03 23:30:08 +0900

    Add proxyResponseTask into CPUUsage.swift

--- src/TrueWidget/swift/WidgetSource/CPUUsage.swift
+++ src/TrueWidget/swift/WidgetSource/CPUUsage.swift
@@ -16,8 +16,17 @@ extension WidgetSource {
 
     @Published public var processes: [[String: String]] = [[:], [:], [:]]
 
-    let timer: AsyncTimerSequence<ContinuousClock>
-    var timerTask: Task<Void, Never>?
+    private let timer: AsyncTimerSequence<ContinuousClock>
+    private var timerTask: Task<Void, Never>?
+
+    struct ProxyResponse: Sendable {
+      let cpuUsage: Double
+      let processes: [[String: String]]
+    }
+
+    private let proxyResponseStream: AsyncStream<ProxyResponse>
+    private let proxyResponseContinuation: AsyncStream<ProxyResponse>.Continuation
+    private var proxyResponseTask: Task<Void, Never>?
 
     init(userSettings: UserSettings) {
       self.userSettings = userSettings
@@ -27,6 +36,10 @@ extension WidgetSource {
         clock: .continuous
       )
 
+      var continuation: AsyncStream<ProxyResponse>.Continuation!
+      proxyResponseStream = AsyncStream { continuation = $0 }
+      proxyResponseContinuation = continuation
+
       timerTask = Task { @MainActor in
         update()
 
@@ -34,29 +47,15 @@ extension WidgetSource {
           update()
         }
       }
-    }
 
-    // Since timerTask strongly references self, make sure to call cancelTimer when CPUUsage is no longer used.
-    func cancelTimer() {
-      timerTask?.cancel()
+      proxyResponseTask = Task { @MainActor in
+        // When resuming from sleep or in similar situations,
+        // responses from the proxy may be called consecutively within a short period.
+        // To avoid frequent UI updates in such cases, throttle is used to control the update frequency.
+        for await proxyResponse in proxyResponseStream._throttle(for: .seconds(1), latest: true) {
+          let cpuUsage = proxyResponse.cpuUsage
+          let processes = proxyResponse.processes
 
-      Task { @MainActor in
-        HelperClient.shared.proxy?.stopTopCommand()
-      }
-    }
-
-    @MainActor
-    private func update() {
-      // To get the CPU utilization of a process (especially kernel_task information),
-      // as far as I've been able to find out, we need to use the results of the top command or need administrator privileges.
-      // Since the top command has a setuid bit and can be used without privilege, we run top command in a helper process and use the result.
-
-      //
-      // CPU Usage
-      //
-
-      HelperClient.shared.proxy?.topCommand { cpuUsage, processes in
-        Task { @MainActor in
           self.usageInteger = Int(floor(cpuUsage))
           self.usageDecimal = Int(floor((cpuUsage) * 100)) % 100
 
@@ -84,6 +83,28 @@ extension WidgetSource {
           }
           self.processes = newProcesses
         }
+      }
+    }
+
+    // Since timerTask strongly references self, make sure to call cancelTimer when CPUUsage is no longer used.
+    func cancelTimer() {
+      timerTask?.cancel()
+      proxyResponseTask?.cancel()
+
+      Task { @MainActor in
+        HelperClient.shared.proxy?.stopTopCommand()
+      }
+    }
+
+    @MainActor
+    private func update() {
+      // To get the CPU utilization of a process (especially kernel_task information),
+      // as far as I've been able to find out, we need to use the results of the top command or need administrator privileges.
+      // Since the top command has a setuid bit and can be used without privilege, we run top command in a helper process and use the result.
+
+      HelperClient.shared.proxy?.topCommand { cpuUsage, processes in
+        self.proxyResponseContinuation.yield(
+          ProxyResponse(cpuUsage: cpuUsage, processes: processes))
       }
     }
   }
